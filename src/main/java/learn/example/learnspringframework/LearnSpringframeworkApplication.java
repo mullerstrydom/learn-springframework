04b945110121b1213e521f0f978baf9fd2a5804b
Muller Strydom <Muller.SA.Strydom@partner.bmwgroup.com>
2022-12-07 09:22:21 +0100

    Empty spring project

---
+++ src/main/java/learn/example/learnspringframework/LearnSpringframeworkApplication.java
@@ -0,0 +1,13 @@
+package learn.example.learnspringframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringframeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringframeworkApplication.class, args);
+    }
+
+}
